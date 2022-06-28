c55c51c833365a2eac1644197b2d2b59b34dabee
anitahx <anitaizzy4@gmail.com>
2022-06-28 02:48:39 -0700

    positive or negative

---
+++ positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * is a positive, zero or negative?
+ */
+
+int main(int a)
+
+{
+	a = 20;
+
+	if (a > 0)
+	{ 
+		printf("a is positive\n");
+	}
+	else if (a == 0)
+	{
+		printf("a is zero\n");
+	}
+	else 
+	{
+		printf("a is negative");
+	}
+	return (0);
+}
+
+
+
