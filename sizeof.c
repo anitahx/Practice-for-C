49dfb58c21e3faf476fe3ab24a031510c5690283
anitahx <anitaizzy4@gmail.com>
2022-06-28 01:56:46 -0700

    size in bytes

---
+++ sizeof.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * prints the size of different data types
+ */
+
+int main()
+
+{
+	printf("size of a char is: %lu bytes\n", sizeof(char));
+	 printf("size of a int is: %lu bytes\n", sizeof(int));
+	  printf("size of a double is: %lu bytes\n", sizeof(double));
+
+	return (0);
+}
