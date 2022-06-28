c993f0fc98debb89ec2ce590432568bcf26b6334
anitahx <anitaizzy4@gmail.com>
2022-06-28 01:30:31 -0700

    compiling files

---
+++ if_statements.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int age;
+int age = 20;
+
+int main(void)
+
+{
+	if (age >=18)
+	{
+		printf("you can buy drinks\n");
+	}
+
+	else
+	{
+		printf("go home you are too young");		
+
+	}
+
+
+	return (0);
+
+}
---
+++ my-first_file.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * my comments for this file
+ */
+
+int main()
+
+{
+	printf("c is fun sometimes\n");
+
+	return (0);
+}
