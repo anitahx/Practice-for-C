74d5f4d5d944850696e91cd96b03e392f6cd165b
anitahx <anitaizzy4@gmail.com>
2022-07-14 01:26:03 -0700

    nested loops algorithm

---
+++ nested-loops.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+
+//algorithm for nested loops
+
+int main(void)
+
+{
+	//initialise variables
+	
+	for(initialisation, condition, ++/--)
+
+		// start internal loop
+		for(initialisation, condition, ++/--
+)
+		{
+			//inner loop statements
+		}
+	//outer loop statements
+}
